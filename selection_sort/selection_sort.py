c1435e9e2e336dbd999b294ad3cd79393973386a
johnkioko10 <john.musyimi@strathmore.edu>
2025-06-20 20:07:52 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,15 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
+    def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    pass
+
+my_list = [6,2,44,5,59,3,1,3]
+selection_sort(my_list)
+print(my_list)
     pass
