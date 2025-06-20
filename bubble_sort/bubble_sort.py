48fd5230534116b1ce46ee3a060eaccde37072fd
johnkioko10 <john.musyimi@strathmore.edu>
2025-06-20 20:06:44 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
+    def bubble_sort(unsorted_list):
+    n = len(unsorted_list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    pass
+
+my_list = [6,2,44,5,59,3,1,3]
+bubble_sort(my_list)
+print(my_list)
     pass
